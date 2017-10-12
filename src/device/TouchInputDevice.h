bd7698238e88382b42c7608247b60d9a9f1c40b5
kunaldawn <kunal.dawn@gmail.com>
2017-10-12 19:53:22 +0530

    added data containers for TouchInputDevice

--- src/device/TouchInputDevice.h
+++ src/device/TouchInputDevice.h
@@ -5,9 +5,32 @@
 #ifndef PROJECT_TOUCHINPUTDEVICE_H
 #define PROJECT_TOUCHINPUTDEVICE_H
 
+#include <string>
+#include <memory>
+#include <map>
 
 class TouchInputDevice {
+protected:
+    int mDeviceFileDescriptor;
+    int mDeviceScore;
+    std::string mDevicePath;
 
+    int mHasMTSlot;
+    int mHasTrackingID;
+    int mHasKeyButtonTouch;
+    int mHasTouchMajor;
+    int mHasTouchWidthMajor;
+    int mHasPressure;
+    int mMinPressure;
+    int mMaxPressure;
+    int mMaxX;
+    int mMaxY;
+    int mMaxContacts;
+    int mMaxTrackingID;
+    int tracking_id;
+    std::map<int, std::shared_ptr> mTouchContacts;
+
+    struct libevdev *evdev = NULL;
 };
 
 
