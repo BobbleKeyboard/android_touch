e238981a8af5fd9472dfcec424885974f5d5203e
kunaldawn <kunal.dawn@gmail.com>
2017-10-24 21:36:53 +0530

    Refactored FileUtils

--- src/utils/FileUtils.h
+++ src/utils/FileUtils.h
@@ -13,39 +13,12 @@ namespace android_touch {
     class FileUtils {
     public:
         /**
-         * Get list of all directories in given path
-         *
-         * @param path Base path
-         * @returns Vector of directory names in given path
-         */
-        static std::vector<std::string> getDirs(std::string path) {
-            std::vector<std::string> list;
-            DIR *dir = opendir(path.c_str());
-            struct dirent *entry = readdir(dir);
-
-            while (entry != NULL) {
-                if (entry->d_type == DT_DIR) {
-                    std::string dirName = std::string(entry->d_name);
-                    if (dirName.find(".") == std::string::npos) {
-                        list.push_back(dirName);
-                    }
-                }
-                entry = readdir(dir);
-            }
-            closedir(dir);
-
-            return list;
-        }
-
-        /**
          * Get list of all files in given path
          *
          * @param path Base path
-         * @param isApplyFilter Boolean value to indicate if filter needs to be applied
-         * @param Filter that needs to be applied if enabled
          * @returns Vector of file names in given path
          */
-        static std::vector<std::string> getFiles(std::string path, bool isApplyFilter, std::string filter) {
+        static std::vector<std::string> getFiles(std::string path) {
             std::vector<std::string> list;
             DIR *dir = opendir(path.c_str());
             struct dirent *entry = readdir(dir);
@@ -53,13 +26,7 @@ namespace android_touch {
             while (entry != NULL) {
                 if (entry->d_type != DT_DIR) {
                     std::string fileName = std::string(entry->d_name);
-                    if (isApplyFilter) {
-                        if (fileName.find(filter) != std::string::npos) {
-                            list.push_back(fileName);
-                        }
-                    } else {
-                        list.push_back(fileName);
-                    }
+                    list.push_back(fileName);
                 }
                 entry = readdir(dir);
             }
