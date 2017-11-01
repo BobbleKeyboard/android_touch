9c477024339bfd658b0117ef2eadecbcc4b89fbb
kunaldawn <kunal.dawn@gmail.com>
2017-11-01 13:00:20 +0530

    Added file headers

--- jni/src/TouchState.h
+++ jni/src/TouchState.h
@@ -1,6 +1,16 @@
-//
-// Created by kunaldawn on 11/10/17.
-//
+/**
+ *                  _           _     _   _                   _
+ *  __ _ _ __   __| |_ __ ___ (_) __| | | |_ ___  _   _  ___| |__
+ * / _` | '_ \ / _` | '__/ _ \| |/ _` | | __/ _ \| | | |/ __| '_ \
+ *| (_| | | | | (_| | | | (_) | | (_| | | || (_) | |_| | (__| | | |
+ * \__,_|_| |_|\__,_|_|  \___/|_|\__,_|  \__\___/ \__,_|\___|_| |_|
+ *
+ * Copyright (c) 2017 Kunal Dawn <kunal.dawn@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ */
 
 #ifndef PROJECT_MULTITOUCHCONTACT_H
 #define PROJECT_MULTITOUCHCONTACT_H
