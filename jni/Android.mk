bfca46c3b09e16a0442e5a0161cf675c3d2b7d05
kunaldawn <kunal.dawn@gmail.com>
2017-11-01 12:48:52 +0530

    Upgraded Android build

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,14 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := android_touch
 
-LOCAL_SRC_FILES := src/TouchInput.cpp \
-                   src/TouchState.cpp \
-                   vendor/libevdev/libevdev.c \
-                   vendor/libevdev/libevdev-names.c \
-                   vendor/libevdev/libevdev-uinput.c \
-                   src/main.cpp
+LOCAL_SRC_FILES := vendor/libevdev/libevdev.c         \
+                   vendor/libevdev/libevdev-names.c   \
+                   vendor/libevdev/libevdev-uinput.c  \
+                   vendor/jsoncpp/jsoncpp.cpp         \
+                   src/Logging.cpp                    \
+                   src/TouchInput.cpp                 \
+                   src/TouchState.cpp                 \
+                   src/Server.cpp                     \
 
-LOCAL_LDLIBS := -latomic
+LOCAL_LDLIBS := -latomic -llog
 
 include $(BUILD_EXECUTABLE)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,4 @@
-APP_STL := gnustl_static
-APP_CPPFLAGS += -std=c++11 -frtti
+APP_STL      := c++_static
+APP_CPPFLAGS += -std=c++11 -frtti -fexceptions
 APP_PLATFORM := android-18
+APP_ABI      := all
